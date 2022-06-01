dfbb4fdba3abec4c69ead07312b5622208fd3970
mike <gnosoir@hotmail.com>
2022-06-01 21:18:55 +0800

    add zero division test

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,5 @@
 from example_package import add_one, time_two, half, square
+import pytest
 
 def test_add_one():
     assert add_one(3) == 4
@@ -8,6 +9,10 @@ def test_time_two():
 
 def test_half():
     assert half(2) == 1
+
+def test_division_zero():
+    with pytest.raises(ZeroDivisionError):
+        1/0
 
 def test_sqaure():
     assert square(3) == 9
